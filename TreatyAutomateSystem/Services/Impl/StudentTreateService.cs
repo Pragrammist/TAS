e2d9d3df0f3174fca90bf73ad83b318baabe072e
MacBook <macbook@MacBook-Air-MacBook.local>
2023-03-25 18:55:49 +0300

    сделал сервис для заполнения индивидульного договора

--- TreatyAutomateSystem/Services/Impl/StudentTreateService.cs
+++ TreatyAutomateSystem/Services/Impl/StudentTreateService.cs
@@ -1,6 +1,7 @@
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Wordprocessing;
 using System.Data;
+using System.Text.RegularExpressions;
 using DocumentFormat.OpenXml;
 
 
@@ -24,6 +25,18 @@ public class StudentTreateService
         public DateTime Start { get; set; }
 
         public DateTime End { get; set; }
+
+
+        public string NaOsnovanii { get; set; } = null!;
+
+
+        public string CompanyName { get; set; } = null!;
+
+
+        public string PracticeDirector { get; set; } = null!;
+
+        
+        public string CompanyRicvizit { get; set; } = null!;
     }
     public class Options
     {
@@ -36,16 +49,38 @@ public class StudentTreateService
         _options = options;
     }
     
+    bool IsMatchedForCompanyData(string text) => 
+            MatchedForCompanyName(text).Success && 
+            MatchedForPracticeDirector(text).Success && 
+            MatchedForNaOsnovanii(text).Success;
+    
+    Match MatchedForCompanyName(string whereInsert) => 
+        new Regex(@"стороны,?\s*и\s*_*\s*").Match(whereInsert);
+    
+
+    Match MatchedForPracticeDirector(string whereInsert) => 
+        new Regex(@"лице\s*_+\s*").Match(whereInsert);
+    
+
+    Match MatchedForNaOsnovanii(string whereInsert) => 
+        new Regex(@"основании\s*_+").Match(whereInsert);
+    
 
     public async Task<Stream> InsertDataToTreate(StudentData student)
     {
-        var doc = GetCopyOfDocument(_options.TreatePlatePath);
-
-        InsertDataToTable(CellsWhereInsertData(doc), PrepareDataToInsert(student));
+        using var doc = GetCopyOfDocument(_options.TreatePlatePath);
+        
+        InsertData(doc, student);
 
         var savedDoc = SaveDoc(doc, student);
 
-        return await GetStreamAndDeleteFile(savedDoc);
+        return new MemoryStream(); //await GetStreamAndDeleteFile(savedDoc);
+    }
+    void InsertData(WordprocessingDocument doc, StudentData student)
+    {
+        InsertCompanyDataToParagraph(doc, student);
+        InsertCompDataToTable(doc, student);
+        InsertStudentDataToTable(doc, student);
     }
     async Task<Stream> GetStreamAndDeleteFile(string path)
     {
@@ -64,37 +99,134 @@ public class StudentTreateService
         return doc;
     }
 
-    void InsertDataToTable(IEnumerable<OpenXmlElement> cells, string[] dataToInsert)
+    void InsertStudentDataToTable(WordprocessingDocument doc, StudentData student)
     {
+        var cells = CellsWhereInsertStudentData(doc);
+        var dataToInsert = PrepareStudentDataToInsert(student);
         // мы горизонтально проходим по таблице и вставляем туда данные
         // в правильном порядке
         int i = 0;
         foreach(var cell in cells)
         {
             var cellParagraph = cell.First(p => p is Paragraph);
-            var d = dataToInsert[i];
-            
-            var text = new Text(d);
-            var run = new Run(text);
-            cellParagraph.AppendChild(run);
+            var run = cellParagraph.First(r => r is Run);
+            var text = new Text(dataToInsert[i]);
+            run.AppendChild(text);
             i++;
         }
     }
-
-
-    IEnumerable<OpenXmlElement> CellsWhereInsertData(WordprocessingDocument doc)
+    void InsertCompDataToTable(WordprocessingDocument doc, StudentData student)
+    {
+        var cell = CellWhereInsertCompData(doc);
+        var par = cell.First(p => p is Paragraph);
+        var run = par.First(r => r is Run);
+        var text = new Text(student.CompanyRicvizit);
+        run.AppendChild(text);
+    }
+    OpenXmlElement CellWhereInsertCompData(WordprocessingDocument doc)
     {
         var body = GetBodyOfDocument(doc);
-        var table =(Table)body.First(e => e is Table);
-        var row = table.Last(r => r is TableRow);
-        var cells = row.Where(s => s.InnerText == string.Empty && s is TableCell);
+        var table =(Table)body.First(e => e is Table && HasCompSignatureData(e));
+        var row = table.First(r => r is TableRow && HasCompSignatureData(r));
+        var cell = row.First(s => s is TableCell && s.InnerText == string.Empty);
+        return cell;
+    }
+   
+
+    bool HasCompSignatureData(OpenXmlElement element) =>
+    IsTableForUserDataMultipleRegex(
+            element.InnerText,
+                @"счет",
+                @"банк",
+                @"почта.*@donstu",
+                @"инн.кпп",
+                @"\d+/\d+",
+                @"счет\s*-\s*\d+",
+                @"счет\s*–\s*\d+",
+                @"телефон.*\d+директор",
+                @"\d+0{3,}\d+");
+
+
+    
+
+
+    void InsertCompanyDataToParagraph(WordprocessingDocument doc, StudentData student)
+    {
+        var body = GetBodyOfDocument(doc);
+
+        var p = (Paragraph)body.First(s => s is Paragraph && IsMatchedForCompanyData(s.InnerText));
+        var run = (Run)p.First(r => r is Run && IsMatchedForCompanyData(r.InnerText));
+
+        ReplaceUnderlineText(run, student);
+    }
+    void ReplaceUnderlineText(Run run, StudentData studentData)
+    {
+        var text = run.First(t => t is Text && IsMatchedForCompanyData(t.InnerText));
+        text.Remove();
+        var newText = new Text(GenerateCompDataText(text.InnerText, studentData));
+        run.AddChild(newText);
+    }
+    
+
+    string GenerateCompDataText(string whereInsert, StudentData studentData)
+    {
+        var forCompNameMatched = MatchedForCompanyName(whereInsert);
+        var forCompNameGeneratedText = InsertDataInsteadOfUnderDash(forCompNameMatched.Value, studentData.CompanyName);
+
+        var forPrDirNameMatched = MatchedForPracticeDirector(whereInsert);
+        var forPrDirNameGeneratedText = InsertDataInsteadOfUnderDash(forPrDirNameMatched.Value, studentData.PracticeDirector);
+
+        var forNaOsnovaniiMatched = MatchedForNaOsnovanii(whereInsert);
+        var forNaOsnovaniiGeneratedText = InsertDataInsteadOfUnderDash(forNaOsnovaniiMatched.Value, studentData.NaOsnovanii);
+
+        var res = whereInsert
+            .Replace(forCompNameMatched.Value, forCompNameGeneratedText)
+            .Replace(forPrDirNameMatched.Value, forPrDirNameGeneratedText)
+            .Replace(forNaOsnovaniiMatched.Value, forNaOsnovaniiGeneratedText);
+        return res;
+    }
+    string InsertDataInsteadOfUnderDash(string whereInsert, string data)
+    {
+        var match = UnderDashMatch(whereInsert);
+
+        var underDash = match.Value;
+
+        return whereInsert.Replace(underDash, $" {data}");
+    }
+    Match UnderDashMatch(string whereInsert) => new Regex(@"\s*_+\s*").Match(whereInsert);
+    IEnumerable<OpenXmlElement> CellsWhereInsertStudentData(WordprocessingDocument doc)
+    {
+        var body = GetBodyOfDocument(doc);
+        var table =(Table)body.First(e => e is Table && HasUserDataSignatureInHeaderOfTable(e));
+        var row = table.First(r => r is TableRow && !HasUserDataSignatureInHeaderOfTable(r));
+        var cells = row.Where(s => s is TableCell && s.InnerText == string.Empty);
         return cells;
     }
+
+    bool HasUserDataSignatureInHeaderOfTable(OpenXmlElement element) => 
+    IsTableForUserDataMultipleRegex(
+            element.InnerText,
+                @"вид\s*практической\s*подготовки",
+                @"срок\s*практической\s*подготовки",
+                @"шифр\s*специальности\s*",
+                @"ф\s*и\s*о\s*обучающегося\s*",
+                @"курс",
+                @"группа",
+                @"окончание",
+                @"начало");
+    bool IsTableForUserDataMultipleRegex(string data, params string[] regexs)
+    {
+        foreach(var regex in regexs)
+            if(new Regex(regex, RegexOptions.IgnoreCase).Match(data).Success)
+                    return true;
+        return false;
+    }
+
     Body GetBodyOfDocument(WordprocessingDocument doc)
         => doc?.MainDocumentPart?.Document.Body ?? throw new NullReferenceException("body of treat is null");
 
 
-    string[] PrepareDataToInsert(StudentData student) => 
+    string[] PrepareStudentDataToInsert(StudentData student) => 
         new string[]
             {
                 student.Speciality,
@@ -111,9 +243,7 @@ public class StudentTreateService
     string SaveDoc(WordprocessingDocument doc, StudentData studentToGenerateDocName)
     {
         var pathToSave = PathToSave(studentToGenerateDocName);
-        var doc2 = doc.SaveAs(pathToSave);
-        doc.Close();
-        doc2.Close();
+        using var doc2 = doc.SaveAs(pathToSave);
         return pathToSave;
     }
     string PathToSave(StudentData student) => 
--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -9,28 +9,52 @@ namespace tests;
 public class UnitTest1
 {
     StudentTreateService.Options opt = new StudentTreateService.Options
-        {
-            FolderPathToSave =  @"/home/f/Documents/dotnet-app/TAS/tests/",
-            TreatePlatePath = @"/home/f/Documents/dotnet-app/TAS/tests/test.docx"
+    {
+        FolderPathToSave =  @"/Users/macbook/Documents/dotnet/TAS/tests",
+        TreatePlatePath = @"/Users/macbook/Documents/dotnet/TAS/tests/Образец индивидуального договора СПО.docx"
 
-        };
+    };
 
     StudentTreateService.StudentData student = new StudentTreateService.StudentData
-        {
-            CourseNum = 3,
-            Group = "547пи-3",
-            Name = "Коваленко Виталий Михайлович",
-            PracticeType = "Производственная практика",
-            Speciality = "09.02.05 Прикладная информатика(по отраслям)",
-            Start = DateTime.Parse("09.03.2022"),
-            End = DateTime.Parse("09.03.2030"), 
-        };
+    {
+        CourseNum = 3,
+
+        Group = "547пи-3",
+
+        Name = "Коваленко Виталий Михайлович",
+
+        PracticeType = "Производственная практика",
+
+        Speciality = "09.02.05 Прикладная информатика(по отраслям)",
+
+        Start = DateTime.Parse("09.03.2022"),
+
+        End = DateTime.Parse("09.03.2030"),
+
+        PracticeDirector = "директора Гундарева Сергея Викторовича",
+
+        CompanyRicvizit = $@"ГБУ ДО РО {"\""}СШОР № 13{"\""}
+                        Юридический адрес
+                        347900, Ростовская область, город Таганрог, ул. Ленина, д. 212-4
+                        ИНН 
+                        6154065344
+                        КПП
+                        615401001
+                        ОГРН
+                        1026102592966
+                        ",
+
+        CompanyName = "ГБУ ДО РО \"СШОР № 13\"",
+
+        NaOsnovanii =  "Устава"
+    };
     
     [Fact]
-    public  void Test1()
+    public  async Task Test1()
     {
-        var p = new OrganizationDataParser();
-        using var file = System.IO.File.OpenRead(@"/Users/macbook/Documents/dotnet/TAS/tests/Реквизиты предприятий.xls");
-        var res = p.ParseExcel(file);
+        var t = new StudentTreateService(opt);
+        using var geneartedTreat = await t.InsertDataToTreate(student);
+
+        
     }
 }
